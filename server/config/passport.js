f2dd3d0d6bba855ae782bad73b2a6edd0a4dafbf
Halo-One <asvicki2002@gmail.com>
2024-08-01 20:27:05 +0530

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://notes-backend-alpha.vercel.app/api/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (token, tokenSecret, profile, done) => {
       try {
