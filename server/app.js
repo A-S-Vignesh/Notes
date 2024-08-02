a5275e17f40ab58e14290358191f621c1a8c6d11
Halo-One <asvicki2002@gmail.com>
2024-08-02 23:32:09 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -33,7 +33,6 @@ connectDB();
 
 
 app.get("/", (req, res) => {
-  console.log("GOOGLE_CALLBACK_URL:", process.env.GOOGLE_CALLBACK_URL);
   res.send("Server is live at " + process.env.FRONTEND_URL + " port");
 })
 
